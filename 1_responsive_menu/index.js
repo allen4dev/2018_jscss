d6a167efd3438c3040333d6e566f7bd658237416
allen4dev <alanaliagadev@gmail.com>
2018-03-03 22:22:57 -0400

    [Add | ToDo] open and close menu | style details

--- 1_responsive_menu/index.js
+++ 1_responsive_menu/index.js
@@ -1,9 +1,17 @@
-const buttons = Array.from(document.querySelectorAll('.menuButton'));
+const menuButtons = Array.from(document.querySelectorAll('.menuButton'));
+const closeButtons = Array.from(document.querySelectorAll('.menuButton-close'));
 
-buttons.forEach(button => button.addEventListener('click', toggleMenu));
+menuButtons.forEach(button => button.addEventListener('click', openMenu));
+closeButtons.forEach(button => button.addEventListener('click', closeMenu));
 
-function toggleMenu(e) {
+function openMenu(e) {
   const nav = document.querySelector(`.${e.target.dataset.nav}`);
 
-  nav.classList.toggle('menu--open');
+  nav.classList.add('menu--open');
+}
+
+function closeMenu(e) {
+  const nav = document.querySelector(`.${e.target.dataset.nav}`);
+
+  nav.classList.remove('menu--open');
 }
