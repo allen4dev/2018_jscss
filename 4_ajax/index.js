d74cd44295067df2b5d56653098a3f42f24cd2e6
allen4dev <alanaliagadev@gmail.com>
2018-03-08 01:08:48 -0400

    [Update] 4_ajax: minor updates

--- 4_ajax/index.js
+++ 4_ajax/index.js
@@ -1,31 +1,39 @@
 const Ajax = (function() {
-  let btnFetch = document.getElementById('button-fetch');
-  let articles = document.querySelector('.Articles');
+  let btnFetch;
+  let articles;
 
   function fetchArticles() {
-    fetch('http://localhost:3000/items')
+    return fetch('http://localhost:3000/items')
       .then(res => res.json())
-      .then(data => {
-        let items = '';
-
-        data.forEach(element => {
-          const article = `
-            <article id="article-${element.id}" className="Article">
-              <h2 className="Article-title">${element.title}</h2>
-              <p className="Article-title">${element.body}</p>
-            </article>
-          `;
-
-          items += article;
-        });
-
-        articles.innerHTML = items;
-      })
       .catch(console.error);
   }
 
+  function appendArticles(data) {
+    let items = '';
+
+    data.forEach(element => {
+      const article = `
+          <article id="article-${element.id}" className="Article">
+            <h2 className="Article-title">${element.title}</h2>
+            <p className="Article-title">${element.body}</p>
+          </article>
+        `;
+
+      items += article;
+    });
+
+    articles.innerHTML = items;
+  }
+
+  function fetchAndAppend() {
+    fetchArticles().then(appendArticles);
+  }
+
   function init() {
-    btnFetch.addEventListener('click', fetchArticles);
+    btnFetch = document.getElementById('button-fetch');
+    articles = document.querySelector('.Articles');
+
+    btnFetch.addEventListener('click', fetchAndAppend);
   }
 
   return {
